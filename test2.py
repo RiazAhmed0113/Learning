d16d53080f9ad0e96992cc18e779f46f5b03c048
RiazAhmed0112 <riaz13579@outlook.com>
2023-10-12 23:06:19 +0100

    still learning...

--- test2.py
+++ test2.py
@@ -1,1 +1,30 @@
 print("Hopefully this works")
+
+# Data Types
+
+# string
+first_name = 'Riaz'
+last_name = 'Ahmed'
+full_name = first_name + " " + last_name
+
+name = 'Riaz'
+print("hello "+full_name)
+# print(type(name))
+
+# integer
+age = 18
+age += + 1
+print("Your age is: " + str(age))
+# print(type(age))
+
+# float
+height = 250.5
+print("Your height is: " + str(height)+"cm")
+# print(type(height))
+
+# boolean
+human = True
+print("Are you a human? " + str(human))
+print(type(human))
+
+
