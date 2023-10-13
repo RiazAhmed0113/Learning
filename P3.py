438da987b421d6edaaf3181d471029b2eac4e31a
RiazAhmed0112 <riaz13579@outlook.com>
2023-10-14 00:07:34 +0100

    Methods/Functions in python

---
+++ P3.py
@@ -0,0 +1,13 @@
+# methods/functions
+name = 'riaz'
+
+print(len(name))  # print the length of the string
+print(name.find("i"))   # Find the index position of string variable
+print(name.capitalize())    # Capitalize the first character of the string
+print(name.upper())     # ALL CAPITALS
+print(name.lower())     # all lower case
+print(name.isdigit())   # return bool value if string is a digit or not/
+print(name.isalpha())   # return bool value if string is all alphabets
+print(name.count('z'))  # count how many characters are in the string variable
+print(name.replace('a', 'z'))   # replace character in a string
+print(name*3)   # repeat print (saves time)
