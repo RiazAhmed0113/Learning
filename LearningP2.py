1b3bf90b45c60e5ca182671dafb5c4e083415131
RiazAhmed0112 <riaz13579@outlook.com>
2023-10-13 23:54:41 +0100

    Multiple Assignment

---
+++ LearningP2.py
@@ -0,0 +1,24 @@
+# Multiple assignment
+
+# name = 'Riaz'
+# age = 18
+# attractive = True
+
+name, age, attractive = 'Riaz', 18, True
+
+print(name)
+print(age)
+print(attractive)
+
+# SpongeBob = 30
+# Sandy = 30
+# Patrick = 30
+# Squidward = 30
+
+SpongeBob = Patrick = Sandy = Squidward = 30
+
+print("spongebob is " + str(SpongeBob))
+print(Patrick)
+print(Sandy)
+print(Squidward)
+
