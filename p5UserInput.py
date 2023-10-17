18e055cca56b75f0f0952dee03741c04f071e73b
RiazAhmed0112 <riaz13579@outlook.com>
2023-10-17 21:20:05 +0100

    User Input

---
+++ p5UserInput.py
@@ -0,0 +1,15 @@
+# User Input
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+height = float(input("How tall are you (in cm) ? "))
+
+age = age + 1
+
+print("Hello "+name)
+print(f"You are {age} years old")
+print(f"You are {height}cm tall")
+
+
+print("You are "+str(age)+" years old")
+
