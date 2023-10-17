2c0940c36f350dc0184922db30d053d885e54590
RiazAhmed0112 <riaz13579@outlook.com>
2023-10-17 21:11:18 +0100

    Type Casting

---
+++ p4TypeCasting.py
@@ -0,0 +1,14 @@
+# Type casting = convert the data type of a value to another data type.
+
+x = 1       # int
+y = 2.0     # float
+z = "3"     # string
+
+z = int(z)
+
+print(x)
+print(y)
+print(z*3)
+
+print("X is "+str(x))
+
