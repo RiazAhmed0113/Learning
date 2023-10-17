60a828a6f0d6674332dfc20d7d9e57362cc37a30
RiazAhmed0112 <riaz13579@outlook.com>
2023-10-17 21:27:20 +0100

    Maths functions

---
+++ p6MathFunctions.py
@@ -0,0 +1,21 @@
+# maths
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+print(round(pi))        # Round a number to the nearest whole integer
+print(math.ceil(pi))    # round a number up to the nearest whole integer
+print(math.floor(pi))   # Round down
+print(abs(pi))          # Absolute value
+print(pow(pi, 2))       # give the variable a power
+print(math.sqrt(pi))    # square root of variable
+
+print(max(x, y, z))     # Find maximum value of given variables
+print(min(x, y, z))     # Find minimum value of given variables
+
+
+
+
