d27587452311fb82cb3476ae1440584244bf78c5
RiazAhmed0112 <riaz13579@outlook.com>
2023-10-19 09:40:31 +0100

    While Loops

---
+++ p10WhileLoops.py
@@ -0,0 +1,12 @@
+# while loop = a statement that will execute its block of code as long as it's condition remains true
+
+#    while 1 == 1:          # Infinite While Loop
+#       print("Help!!!!")
+
+name = ""   # empty string/variable
+
+while len(name) == 0:
+    name = input("Enter your name: ")
+
+print("Hello "+name)    # stopping condition
+
