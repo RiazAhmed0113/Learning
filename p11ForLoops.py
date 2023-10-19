a055ccb5f3daa5e275362699d75a913a4bfe9514
RiazAhmed0112 <riaz13579@outlook.com>
2023-10-19 09:48:28 +0100

    For Loops

---
+++ p11ForLoops.py
@@ -0,0 +1,23 @@
+import time
+# for loop = a statement that will execute its block of code a limited amount of times
+# while loop = unlimited
+# for loop = limited
+
+# for index in range(10):
+#    print(index+1)
+
+# for index in range(50,100,2):
+#    print(index)
+
+# for i in "Riaz Ahmed":
+#    print(i)
+
+for seconds in range(10, 0, -1):
+    print(seconds)
+    time.sleep(1)
+print("Happy New Year!!!")
+
+
+
+
+
