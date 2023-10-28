bc0f60d01018e18c02709fd175ae4926cc4be605
RiazAhmed0112 <riaz13579@outlook.com>
2023-10-28 22:33:34 +0100

    more to learn with practice of functions...

--- LearnFunctions.py
+++ LearnFunctions.py
@@ -15,7 +15,30 @@ def display_invoice(username, amount, due_date):
 display_invoice("Riaz", 500, "1/11")
 
 
+# return = statement used to end a function and send a result back to the caller
 
+def add(x, y):
+    z = x + y
+    return z
 
+def subtract(x, y):
+    z = x - y
+    return z
+
+def multiply(x, y):
+    z = x * y
+    return z
+
+print(add(1, 2))
+print(subtract(1, 2))
+print(multiply(1, 2))
+
+def create_name(first_name, last_name):
+    first_name = first_name.capitalize()
+    last_name = last_name.capitalize()
+    return first_name + " " + last_name
+
+full_name = create_name("riaz", "ahmed")
+print(full_name)
 
 
